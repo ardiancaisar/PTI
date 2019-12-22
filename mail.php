74cd521509a53b19613c1206c073e01ec4af0e37
ardiancaisar <ardiancaisar.crew@gmail.com>
2019-12-22 22:38:59 +0700

    commit

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "mohinpatwary42@gmail.com";
+        $recipient = "ahp3695@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
